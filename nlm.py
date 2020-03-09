0d511d1e7148c6e000713704f131864acd776960
James Goodall <james@frizlette.me>
2020-03-09 15:14:49 +0000

    work

--- nlm.py
+++ nlm.py
@@ -1,19 +1,24 @@
-import cv2
+import cv2, sys
 
 ##############################################################
 
 # These parameter values are indicative. You should choose your own
 # according to properties of the method you want to demonstrate
-
 h = 5
 templateWindowSize = 7
 searchWindowSize = 21
 
+
+if len(sys.argv) == 5:
+    h = int(sys.argv[2])
+    templateWindowSize = int(sys.argv[3])
+    searchWindowSize = int(sys.argv[4])
+
+
 ##############################################################
 
-import sys
 
-img = cv2.imread('Images/'+sys.argv[1])
+img = cv2.imread(sys.argv[1])
 
 dst = cv2.fastNlMeansDenoisingColored(img, None, h, h, templateWindowSize, 21)
 
